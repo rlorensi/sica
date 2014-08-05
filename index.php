59c115caf2460a7b83947595d4332bc33091dd15
rlorensi <ruilorensi@gmail.com>
2014-08-05 10:31:43 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Ella Cabelos</title>
+
+        <link href='http://fonts.googleapis.com/css?family=Duru+Sans' rel='stylesheet' type='text/css'>
+        <link href="estilo/estilo.css" type="text/css" rel="stylesheet"/>
+        <script type="text/javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/javascript.js"></script>
+
+    </head>
+    <body>
+        <div id="container">
+            <div id="header">
+                <span id="fone">55-32129027</span>
+                <nav id="menu">
+                    <ul>
+                        <li><a href="#top">INÍCIO</a></li>
+                        <li><a href="#servicos">O QUE FAZEMOS</a></li>
+                        <li><a href="#top-nos">SOBRE NÓS</a></li>
+                        <li><a href="#contato">CONTATO</a></li>
+                    </ul>
+                </nav>
+                <img src="" id="facebook">
+            </div><!--FIM DA HEADER-->
+
+            <div id="top"></div>
+
+            <div id="inicio">
+                <img src="img/ensaio.png" id="1">
+                <img src="img/ella.png" id="2">
+            </div>
+            <div id="container-coluna">
+                <div id="coluna">
+
+                    <img src="img/modelo1.png">
+                    <h2>CEVADIS</h2>
+                    <p>
+                        Ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. 
+                        Pra lá, depois divoltis porris, paradis. Paisis, filhis, espiritis santis.
+                    </p>
+                </div>
+                <div id="coluna">
+                    <img src="img/modelo2.png">
+                    <h2>MALTIS</h2>
+                    <p> 
+                        Ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. 
+                        Pra lá, depois divoltis porris, paradis. Paisis, filhis, espiritis santis.
+                    </p>
+                </div>
+                <div id="coluna">
+                    <img src="img/modelo3.png">
+                    <h2>LUPULIS</h2>
+                    <p> 
+                        Ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. 
+                        Pra lá, depois divoltis porris, paradis. Paisis, filhis, espiritis santis.
+                    </p>
+                </div>
+                <div id="coluna">
+                    <img src="img/modelo5.png">
+                    <h2>AGUIS</h2>
+                    <p> 
+                        Ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. 
+                        Pra lá, depois divoltis porris, paradis. Paisis, filhis, espiritis santis.
+                    </p>
+                </div>
+            </div>
+            <div style="clear: both; height: 400px;"></div>
+
+            <div id="servicos"></div><!--FIM DA DIV SERVICOS-->
+
+            <div id="top-nos"></div>
+
+            <div id="nos">
+                <div id="profissional">
+                    <h2>Ella</h2>
+                    <p>Ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. 
+                        Pra lá, depois divoltis</p>
+                </div>
+                <span id="divisor"></span>
+                <div id="profissional">
+                    <h2>Patrícia</h2>
+                    <p>Ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. 
+                        Pra lá, depois divoltis</p>
+                </div>
+                <span id="divisor"></span>
+                <div id="profissional">
+                    <h2>Sissa</h2>
+                    <p>Ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. 
+                        Pra lá, depois divoltis</p>
+                </div>
+            </div><!--FIM DA DIV SOBRE NOS-->
+
+            <div style="clear: both; height: 400px;"></div>
+
+            <div id="contato">
+                <h1>Contato</h1>
+                <div id="mapa">
+                    <h2><b>55-32129027</b></h2>
+                    <p><b>contato@ellacabelos.com</b></p> 
+                    <p>Rua Armin schivarcz,100</p> 
+                    <p>Tancredo Neves - SM</p> 
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d55454.89574785786!2d-53.88000160000002!3d-29.692780549999988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sRUA.+Armin+Schvarcz%2C+100+-+Tancredo+Neves%2C+Santa+Maria+-+RS!5e0!3m2!1spt-BR!2sbr!4v1406660855649" width="600" height="450" frameborder="0" style="border:0"></iframe>
+                </div>
+                <div id="form-contato">
+                    <h2>Deixe um recado</h2>
+                    <p>Nome</p>
+                    <input type="text" />
+                    <p>Telefone</p>
+                    <input type="text" />
+                    <p>E-mail</p>
+                    <input type="text" />
+                    <p>Mensagem</p>
+                    <input type="text" />
+                </div>
+            </div><!--FIM DA DIV CONTATO-->
+
+            <div id="footer">
+                RODAPÉ
+            </div><!--FIM DA FOOTER-->
+
+        </div><!--FIM DA CONTAINER-->   
+    </body>
+</html>
